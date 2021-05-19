e3df271b36349f2294f5ff18e9ed3fed20e149e5
walterdegraaf <wdegraaf93@gmail.com>
2021-05-19 14:07:02 +0200

    Create IncativeUsers90Days.ps1

---
+++ IncativeUsers90Days.ps1
@@ -0,0 +1,27 @@
+#Import MSOline Module
+ import-module MSOnline
+ #Import Exchange Online Module
+ Import-Module ExchangeOnlineManagement
+
+#Set admin UPN
+$UPN = 'adm-walter@mc1s.com'
+
+#This connects to Azure Active Directory & Exchange Online
+Connect-MsolService
+$EXOSession = New-ExoPSSession -UserPrincipalName $UPN
+Import-PSSession $EXOSession -AllowClobber
+
+$startDate = (Get-Date).AddDays(-90).ToString('MM/dd/yyyy')
+$endDate = (Get-Date).ToString('MM/dd/yyyy')
+
+$allUsers = @()
+$allUsers = Get-MsolUser -All -EnabledFilter EnabledOnly | Select UserPrincipalName
+
+$loggedOnUsers = @()
+$loggedOnUsers = Search-UnifiedAuditLog -StartDate $startDate -EndDate $endDate -Operations UserLoggedIn, PasswordLogonInitialAuthUsingPassword, UserLoginFailed -ResultSize 5000
+
+$inactiveInLastThreeMonthsUsers = @()
+$inactiveInLastThreeMonthsUsers = $allUsers.UserPrincipalName | where {$loggedOnUsers.UserIds -NotContains $_}
+
+Write-Output "The following users have no logged in for the last 90 days:"
+Write-Output $inactiveInLastThreeMonthsUsers
