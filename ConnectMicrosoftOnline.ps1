7936639b08b54c964f73409e26195b10ccaea5af
walterdegraaf <wdegraaf93@gmail.com>
2021-05-19 14:07:26 +0200

    Update ConnectMicrosoftOnline.ps1

--- ConnectMicrosoftOnline.ps1
+++ ConnectMicrosoftOnline.ps1
@@ -1,30 +1,56 @@
-function ConnectExchange {
-    param (
-        $UPN
-    )
-    if ((Get-Module) -notcontains "ExchangeOnlineManagement") {
-        install-module ExchangeOnlineManagement
-        Import-Module ExchangeOnlineManagement
-    }  
-    Connect-ExchangeOnline -UserPrincipalName $UPN -ShowProgress $true
+function ConnectAzure {
+$AzureModule = Get-Module -Name "AzureAD" -ErrorAction SilentlyContinue
+$ImportAzureModule = Import-Module "AzureAD" -ErrorAction SilentlyContinue -ErrorVariable ImportError
+    if (!$AzureModule) {
+        $ImportAzureModule                      
+        if ($ImportError) {
+            Write-Warning "Module is not installed..."
+            Write-Host "Installing the module..."
+            install-module AzureAD
+        }
+        Write-Host "Importing the module..."
+        $ImportAzureModule
+        
+    }
+    
+    Write-Host -ForegroundColor "Green" "Connecting to AzureAD..."
+    Connect-AzureAD
 }
 
 function ConnectMSOL {
-    param (
-        $UPN
-    )
-    if ((Get-Module) -notcontains "MSOnline") {
-        install-module MSOnline
-        import-module MSOnline    
+    $MSOLModule = Get-Module -Name "MSOnline" -ErrorAction SilentlyContinue
+    $ImportMSOLModule = Import-Module "MSOnline" -ErrorAction SilentlyContinue -ErrorVariable ImportError
+        if (!$MSOLModule) {
+            $ImportMSOLModule                     
+            if ($ImportError) {
+                Write-Warning "Module is not installed..."
+                Write-Host "Installing the module..."
+                install-module MSOnline
+            }
+            Write-Host "Importing the module..."
+            $ImportMSOLModule
+            
+        }
+        
+        Write-Host -ForegroundColor "Green" "Connecting to MSOnline..."
+        Connect-MSOLservice
     }
-    Connect-MSOLservice
-}
 
-function ConnectAzure {
-
-    if ((Get-Module) -notcontains "AzureAD") {
-        install-module AzureAD
-        Import-Module AzureAD -ErrorAction SilentlyContinue
+function ConnectExchange {
+    $ExchangeModule = Get-Module -Name "ExchangeOnlineManagement" -ErrorAction SilentlyContinue
+    $ImportExchangeModule = Import-Module "ExchangeOnlineManagement" -ErrorAction SilentlyContinue -ErrorVariable ImportError
+        if (!$ExchangeModule) {
+            $ImportExchangeModule                     
+            if ($ImportError) {
+                Write-Warning "Module is not installed..."
+                Write-Host "Installing the module..."
+                install-module ExchangeOnlineManagement
+            }
+            Write-Host "Importing the module...."
+            $ImportExchangeModule
+            
+        }
+        
+        Write-Host -ForegroundColor "Green" "Connecting to Exchange Online..."
+        Connect-ExchangeOnline
     }
-    Connect-AzureAD
-}
